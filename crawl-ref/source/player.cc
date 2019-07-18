b5f8946e643b1138be6a28a8a56e622262d5b430
advil <rawlins@gmail.com>
2019-07-18 10:55:45 -0400

    Separate unique vault tracking for abyss from everything else
    This commit doesn't affect vaults that place only in one or the other,
    but for vaults that place in both and are supposed to be unique (e.g.
    hangedman_summoners_spam, lemuel_flamethrower_hall), these vaults can
    now in principle independently place twice, once in the abyss and once
    outside. (The behavior of unseen vaults in the abyss isn't changed, so
    if they aren't seen in the abyss, they can keep re-placing there.) This
    is consistent with abyss's flavor as a shadowy mirror of the outside
    world.
    
    Something like this is necessary for seed stability, as long as regular
    levelgen can be interleaved with the abyss: maps that can place in both
    abyss and elsewhere and are unique in some way can interfere with
    incremental pregeneration if they place in the abyss and are seen there,
    and then would try to place in an as-yet-ungenerated level. There aren't
    a lot of vaults that this affects, but there are a few, mostly placing
    in depths and elf.
    
    Abyss should be the only branch that this problem applies to. Pan is
    handled differently, by forcing everything else to pregenerate, and zigs
    + bazaars have non-overlapping vault lists (plus zigs will force
    complete pregeneration as well). The implementation is therefore a bit
    ad hoc in terms of serializing, though I have generalized how unique
    vault lists are accessed.
    
    Semi-related debugging changes: This commit also affects how vaults are
    shown in morgues, and might be kind of spammy for abyss / pan (it shows
    every vault that ever places), so this part may need further tweaking.
    There's also a bit of useful debugging infrastructure.

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -5084,6 +5084,8 @@ player::player()
 
     uniq_map_tags.clear();
     uniq_map_names.clear();
+    uniq_map_tags_abyss.clear();
+    uniq_map_names_abyss.clear();
     vault_list.clear();
 
     global_info = PlaceInfo();
